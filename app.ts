04682cdeaf75436fd2c453977758819723fdcda2
KiKiKi <chaika.design@gmail.com>
2024-09-22 00:46:28 +0900

    :gem: Get answer with Gemini AI

--- app.ts
+++ app.ts
@@ -2,13 +2,18 @@
 const scriptProperties = PropertiesService.getScriptProperties();
 const CHANNEL_TOKEN =
   scriptProperties.getProperty("LINE_CHANNEL_TOKEN") ?? undefined;
+const GEMINI_API_KEY =
+  scriptProperties.getProperty("GEMINI_API_KEY") ?? undefined;
 
 // API Endpoint
 const LINE_REPLY_API = "https://api.line.me/v2/bot/message/reply";
 // ref. https://developers.line.biz/ja/reference/messaging-api/#display-a-loading-indicator
 const LINE_CHAT_LOADING_API = "https://api.line.me/v2/bot/chat/loading/start";
 
-function doPost(e) {
+// Gemini API
+const GEMINI_API = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`;
+
+async function doPost(e) {
   const json = JSON.parse(e.postData.contents);
 
   // Ref: https://developers.line.biz/ja/reference/messaging-api/#message-event
@@ -27,8 +32,12 @@ function doPost(e) {
     showLoading({ userID });
   }
 
-  // :parrot: Reply
+  // Ask Gemini
+  const answerText = await askGemini({
+    prompt: messageText,
+  });
 
+  // Reply
   const option = {
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
@@ -40,7 +49,7 @@ function doPost(e) {
       messages: [
         {
           type: "text",
-          text: messageText,
+          text: answerText,
         },
       ],
     }),
@@ -66,4 +75,36 @@ const showLoading = ({ userID }) => {
   };
 
   UrlFetchApp.fetch(LINE_CHAT_LOADING_API, option);
+};
+
+const askGemini = async ({ prompt }) => {
+  if (!prompt || prompt.length < 3) {
+    return "回答できませんでした";
+  }
+
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    payload: JSON.stringify({
+      contents: [
+        {
+          parts: [
+            {
+              text: `次の質問に140文字以内で回答してください。
+                質問: ${prompt}`,
+            },
+          ],
+        },
+      ],
+    }),
+  };
+
+  const res = UrlFetchApp.fetch(GEMINI_API, options);
+  const json = JSON.parse(res.getContentText());
+  // console.log(json.candidates[0].content.parts);
+  const text = json.candidates[0].content.parts[0].text;
+
+  return text ?? "回答できませんでした";
 };
